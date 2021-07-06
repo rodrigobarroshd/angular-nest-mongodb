d630ba99e7432f0620fe6c6e58ed4ebcd681cd61
Rodrickhd <brodrigo87@gmail.com>
2021-07-05 22:36:26 -0300

    modify menu bar

--- ionic/src/app/app.component.ts
+++ ionic/src/app/app.component.ts
@@ -17,30 +17,3 @@ export class AppComponent {
 
   }
 }
-
-// import { Component } from '@angular/core';
-// import { MenuController } from '@ionic/angular';
-
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: 'app.component.html',
-//   styleUrls: ['app.component.scss'],
-// })
-// export class AppComponent {
-
-// constructor(private menu: MenuController) { }
-
-//   openFirst() {
-//     this.menu.enable(true, 'first');
-//     this.menu.open('first');
-//   }
-
-//   openEnd() {
-//     this.menu.open('end');
-//   }
-
-//   openCustom() {
-//     this.menu.enable(true, 'custom');
-//     this.menu.open('custom');
-//   }
-// }
