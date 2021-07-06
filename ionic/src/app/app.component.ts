e3b82ae68c979da9e14f56518f8c888a27022219
Rodrickhd <brodrigo87@gmail.com>
2021-07-05 23:46:51 -0300

    changes name navbar

--- ionic/src/app/app.component.ts
+++ ionic/src/app/app.component.ts
@@ -9,9 +9,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public appPages = [
     // { title: 'Home', url: 'home', icon: 'home' },
-    { title: 'Product', url: '/product', icon: 'settings' },
-    { title: 'Create', url: '/product/create', icon: 'settings' },
-    { title: 'Charts', url: '/charts', icon: 'images' }
+    { title: 'Dados Médicos', url: '/product', icon: 'settings' },
+    { title: 'Formulario', url: '/product/create', icon: 'settings' },
+    { title: 'Graficos', url: '/charts', icon: 'images' }
   ];
   constructor() {
 
